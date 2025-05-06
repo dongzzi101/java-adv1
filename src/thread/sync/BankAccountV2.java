e8e0b6bfc7ffaf139ca7ed9e503470c600c90a50
dongwook <dongzzi101@gmail.com>
2025-05-06 20:43:24 +0900

    [synchronized 코드 블럭} :
    synchronized 장점이자 단점은 한 번에 하나의 쓰레드만 실행 가능 → 여러 쓰레드가 동시에 실행하지 못하므로 전체로 보면 성능이 떨어질 수 있음 → 꼭 필요한 곳으로 한정해서 설정 해야함.
    
    동기화 사용하면 다음 문제들 해결 가능
    
    - 경합 조건(race condition) : 두 개 이상의 쓰레드가 경쟁적으로 동일한 자원을 수정 시 발생하는 문제
    - 데이터 일관성 : 여러 쓰레드가 동시에 읽고 쓰는 데이터의 일관성을 유지

--- src/thread/sync/BankAccountV2.java
+++ src/thread/sync/BankAccountV2.java
@@ -14,10 +14,9 @@ public class BankAccountV2 implements BankAccount {
     @Override
     public synchronized boolean withdraw(int amount) {
         log("거래 시작 : " + getClass().getSimpleName());
-        // 잔고가 출금액 보다 적으면 return false
 
+        // 임계 영역 시작
         log("[검증 시작] 출금액: " + amount + ", 잔액 :" + balance);
-
         if (balance < amount) {
             log("[검증 실패] 출금액: " + amount + ", 잔액 :" + balance);
             return false;
@@ -26,10 +25,10 @@ public class BankAccountV2 implements BankAccount {
         // 잔고가 출금액 보다 많으면 return true
         log("[검증 완료] 출금액: " + amount + ", 잔액 :" + balance);
         sleep(1000); // 출금 걸리는 시간으로 가정
-
         balance -= amount;
-
         log("[출금 완료] 출금액: " + amount + ", 잔액 :" + balance);
+        // 임계 영역 종료
+
         log("거래 종료");
         return true;
     }
